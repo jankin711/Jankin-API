2de8e590c6cec497d2b251414a1fc44bb8fafc21
jankin <781524813@qq.com>
2023-11-15 22:21:45 +0800

    新增网关项目

---
+++ jankin-api-gateway/src/test/java/com/jankin/jankinapigateway/JankinApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jankin.jankinapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankinApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
