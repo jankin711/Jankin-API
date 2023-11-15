ca3ad864b49bd469ada357f714e5824fa6bc1240
jankin <781524813@qq.com>
2023-11-15 22:33:00 +0800

    修改请求地址为网关请求地址

--- jankin-api-client-sdk/src/main/java/com/jankin/jankinapiclientsdk/client/JankinApiClient.java
+++ jankin-api-client-sdk/src/main/java/com/jankin/jankinapiclientsdk/client/JankinApiClient.java
@@ -20,6 +20,8 @@ import static com.jankin.jankinapiclientsdk.utils.SignUtils.genSign;
  */
 public class JankinApiClient {
 
+    private static final String GATEWAY_HOST = "http://localhost:8090";
+
     private String accessKey;
 
     private String secretKey;
@@ -34,7 +36,7 @@ public class JankinApiClient {
         // 可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
         HashMap<String, Object> paramMap = new HashMap<>();
         paramMap.put("name", name);
-        String result = HttpUtil.get("http://localhost:8123/api/name/", paramMap);
+        String result = HttpUtil.get(GATEWAY_HOST + "/api/name/", paramMap);
         System.out.println(result);
         return result;
     }
@@ -44,7 +46,7 @@ public class JankinApiClient {
         // 可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
         HashMap<String, Object> paramMap = new HashMap<>();
         paramMap.put("name", name);
-        String result = HttpUtil.post("http://localhost:8123/api/name/", paramMap);
+        String result = HttpUtil.post(GATEWAY_HOST + "/api/name/", paramMap);
         System.out.println(result);
         return result;
     }
@@ -73,7 +75,7 @@ public class JankinApiClient {
      */
     public String getUserNameByPost(User user) {
         String json = JSONUtil.toJsonStr(user);
-        HttpResponse httpResponse = HttpRequest.post("http://localhost:8123/api/name/user")
+        HttpResponse httpResponse = HttpRequest.post(GATEWAY_HOST + "/api/name/user")
                 .addHeaders(getHeaderMap(json))
                 .body(json)
                 .execute();
