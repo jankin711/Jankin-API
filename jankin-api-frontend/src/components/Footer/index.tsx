85e4e033349a0b3b00dacadfe3cca4d559459ed8
jankin <781524813@qq.com>
2023-11-22 01:39:29 +0800

    优化界面

--- jankin-api-frontend/src/components/Footer/index.tsx
+++ jankin-api-frontend/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { DefaultFooter } from '@ant-design/pro-components';
 import '@umijs/max';
 import React from 'react';
 const Footer: React.FC = () => {
-  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const defaultMessage = 'Jankin出品';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
@@ -12,22 +12,22 @@ const Footer: React.FC = () => {
       }}
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
-        {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
+        // {
+        //   key: 'Ant Design Pro',
+        //   title: 'Ant Design Pro',
+        //   href: 'https://pro.ant.design',
+        //   blankTarget: true,
+        // },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/Jankin711',
           blankTarget: true,
         },
         {
           key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          title: 'Jankin API',
+          href: 'https://github.com/jankin711/jankin-api',
           blankTarget: true,
         },
       ]}
